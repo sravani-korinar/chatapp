6ed882996c61886b2ada8776325892e55eeb5977
sravanijava <sravanigoud235@gmail.com>
2021-06-28 13:11:37 +0530

    omdify1

--- src/main/java/com/chat/chatapp/chatcontroller.java
+++ src/main/java/com/chat/chatapp/chatcontroller.java
@@ -3,9 +3,11 @@ package com.chat.chatapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.chat.chatapp.chatservice.chatser;
 
-@Controller
+@RestController
 public class chatcontroller {
 	@Autowired
 	private  chatser service;
